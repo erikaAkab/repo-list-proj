8540120f50f967bb2b8d6512dad50f9d2cc25803
Erika Akabane <midori.akabane@gmail.com>
2018-10-12 16:35:18 -0300

    Update git-hub-login layout and create git hub login button callback

--- src/app/git-hub-login/git-hub-login.component.ts
+++ src/app/git-hub-login/git-hub-login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { GitHubService } from '../github.service';
+import { environment } from './../../environments/environment'
 
 @Component({
   selector: 'app-git-hub-login',
   templateUrl: './git-hub-login.component.html',
   styleUrls: ['./git-hub-login.component.css']
 })
+
 export class GitHubLoginComponent implements OnInit {
+  constructor(private gitHubService: GitHubService, private router: Router, private title: Title) { }
 
-  constructor() { }
+  ngOnInit() {
+    if (this.gitHubService.getSignedStatus() === true) {
+      this.router.navigate(['repository-list']);
+    } else {
+      this.title.setTitle('GithubRepositoryList - Autenticação');
+    }
+  }
 
-  ngOnInit() {}
-
-  requestLogin() {}
+  requestLogin() {
+    location.href = `https://github.com/login/oauth/authorize?client_id=${
+        environment.CLIENT_ID
+    }&scope=public_repo&redirect_uri=${location.protocol}//${location.host}/loading`;
+  }
 }
