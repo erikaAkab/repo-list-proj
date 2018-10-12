0d0b2081b91d32cce29af8d0c8fe7eda435c19be
Erika Akabane <midori.akabane@gmail.com>
2018-10-12 16:23:12 -0300

    Create Auth Guard to repository-list route

---
+++ src/app/auth-guard.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, ActivatedRouteSnapshot, Route, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { GitHubService } from './github.service';
+
+import * as VanillaToasts from 'vanillatoasts';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private gitHubService: GitHubService, private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const status = this.gitHubService.getSignedStatus();
+
+    if (status === false) {
+        setTimeout(
+            () =>
+                VanillaToasts.create({
+                    title: 'Atenção!',
+                    text: `Para acessar "${next.routeConfig.path}" é necessário autenticação.`,
+                    type: 'warning',
+                    timeout: 3500
+                }),
+            1000
+        );
+  
+        this.router.navigate(['git-hub-login']);
+    } else {
+        return status;
+    }
+  }
+
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    return this.gitHubService.getSignedStatus();
+  }
+}
