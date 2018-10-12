6d663a09faec4b1c8b9ad3169aac8ac168f5580c
Erika Akabane <midori.akabane@gmail.com>
2018-10-12 17:08:23 -0300

    Create repository list component

---
+++ src/app/repository-list/repository-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { GitHubService } from '../github.service';
+
+import { Repository } from '../models/repository.model';
+
+import * as VanillaToasts from 'vanillatoasts';
+
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-repository-list',
+  templateUrl: './repository-list.component.html',
+  styleUrls: ['./repository-list.component.css']
+})
+
+export class RepositoryListComponent implements OnInit {
+  public repositories: Array<Repository> = [];
+  public repositoriesToShow: Array<Repository> = [];
+
+  constructor(private title: Title, private githubService: GitHubService) {}
+
+  ngOnInit() {
+      this.title.setTitle('GitHub - Meus Repositórios');
+
+      this.githubService.getRepositoryList().subscribe(
+          (repositories: Array<Repository>) => {
+              Object.assign(this.repositories, repositories);
+              Object.assign(this.repositoriesToShow, repositories);
+          },
+          () => {
+              this.githubService.signOut();
+              VanillaToasts.create({
+                  title: 'Erro!',
+                  text: `Não foi possivel carregar a lista de repositórios.`,
+                  type: 'error',
+                  timeout: 3500
+              });
+          }
+      );
+  }
+}
