defcc66ff7f70bd73c49a0a93c608acfbab75c61
Erika Akabane <midori.akabane@gmail.com>
2018-10-12 16:25:11 -0300

    Update on header css and create sign in button callback

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GitHubService } from '../github.service';
+
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
     public userSigned: boolean;
     public headerDescription: string;
 
-    constructor() {}
+    constructor(private gitHubService: GitHubService) {}
 
-  ngOnInit() {
+    ngOnInit() {
+        this.gitHubService.loginStatus.subscribe((status) => {
+            this.userSigned = status;
+        });
     }
 
     getHeaderTitle() {
-        this.headerDescription = "Temporario"
+        if(this.userSigned) {
+            this.headerDescription = "Lista de repositórios";
+        } else {
+            this.headerDescription = "Acesso a lista de repositórios";
+        }
         return this.headerDescription;
-  }
+    }
 
     signOut() {
+        this.gitHubService.signOut();
     }
 }
