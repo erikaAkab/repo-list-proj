3d574af1168a1f60c4c1f231b13ed6a80b42f5f8
Erika Akabane <midori.akabane@gmail.com>
2018-10-16 20:35:37 -0300

    Update test files

--- src/app/auth-guard.guard.spec.ts
+++ src/app/auth-guard.guard.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AuthGuard } from './auth-guard.guard';
+import { Router } from '@angular/router';
+import { GitHubService } from './github.service';
+
 
 describe('AuthGuard', () => {
+  const router = {
+    navigate: () => ({})
+  };
+
+  const githubService = {
+    getSignedStatus: () => ({})
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        AuthGuard,
+        { provide: Router, useValue: router },
+        { provide: GitHubService, useValue: githubService },
+      ]
     });
   });
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
+  it('should create', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
 });
--- src/app/github.service.spec.ts
+++ src/app/github.service.spec.ts
@@ -1,9 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { GitHubService } from './github.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 describe('GitHubService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const router = {
+    navigate: () => ({})
+  };
+
+  const httpClient = {
+    get: () => ({}),
+    post: () => ({})
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    schemas: [NO_ERRORS_SCHEMA],
+    providers: [
+      { provide: Router, useValue: router },
+      { provide: HttpClient, useValue: httpClient },
+    ]
+  }));
 
   it('should be created', () => {
     const service: GitHubService = TestBed.get(GitHubService);
